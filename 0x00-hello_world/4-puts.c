ea05b1ace51ebf445ba6fe817fddd312b2f93dd4
Peternab8 <Peternabilg@gmail.com>
2023-07-17 08:53:29 -0700

    a

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ *main - Entry pouint
+ *
+ *Description: A c Program that prints with put fanction
+ *
+ *Return: Always 0 (Success)
+*/
+int main(void)
+{        
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}	
